847502e39545a7cc77e1558425e7b7218e3a3415
minseok <minseok@localhost.localdomain>
2019-08-18 20:51:07 +0000

    save

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 import src.model as sm
 import src.api as sa
 
+def login(id,passwd):
+    token = token = sa.get_token(id,passwd)
+    names, uuid = sa.get_projectID(token)
+    #TODO : UI ask specific proejct name
+    print("PLEASE SELECT PROJECT NAME")
+    print(names)
+    index = int(input())
+    token = sa.get_other_token(id,passwd,uuid[index])
+    print(sa.get_flavor_info(token, uuid[index]))
+    return token
+
+def server_info(token):
+    servers_names, servers_uuid = sa.get_server_list(token)
+    # TODO : UI ask specific server name
+    print("PLEASE SELECT SERVER NAME")
+    print(servers_names)
+    index = int(input())
+    server = sa.get_server_info(token, servers_uuid[index])
+    print(server['server'])
+    return servers_uuid[index]
+    #return data
+
+def get_resource_info(token,uuid):
+    print("get_resource_info")
+    res = sa.get_resource_list(token,uuid)
+    print(res)
+    return sa.get_mesuare_list(token, res)
+
+
 if __name__ =='__main__':
-    token = sa.get_token('admin','devstack')
+    token = login('admin','devstack')
+    instance_uuid = server_info(token)
+    cpu, memory, disk = get_resource_info(token, instance_uuid)
     model = sm.load_model('model/model')
-    sm.predict( 10, 10, 10, 10, model)
+    print("PLEASE INPUT RATING DATA")
+    rating = int(input())
+    sm.predict( cpu, memory, disk, rating, model)
+
 
--- src/api.py
+++ src/api.py
@@ -4,37 +4,30 @@ import datetime
 
 url_base = "http://localhost/"
 
-token = ""
-server_uuid = []
-server_names = []
-
-
-def get_projectID():
-    global token
+def get_projectID(token):
     url = url_base + "identity/v3/auth/projects"
     headers = {'Content-Type': 'application/json', 'X-Auth-Token': token}
     res = requests.get(url, headers=headers)
     body = res.json()
-    projects = [ x['id'] for x in body['projects'] if not x['name'] == "invisible_to_admin"]
-    return projects
+
+    projects_name = [x['name'] for x in body['projects'] if not x['name'] == "invisible_to_admin"]
+    projects_uuid = [ x['id'] for x in body['projects'] if not x['name'] == "invisible_to_admin"]
+
+    return projects_name, projects_uuid
 
 # for ask what kinds of instances admin control on dashboard
-def get_server_list():
-    global server_uuid
-    global server_names
-    global token
+def get_server_list(token):
+    server_uuid = []
+    server_names = []
     url = url_base + "compute/v2.1/servers"
     headers = {'Content-Type': 'application/json', 'X-Auth-Token': token}
     res = requests.get(url, headers=headers)
     body = res.json()
-
     server_uuid = [ x['id'] for x in body['servers']]
     server_names = [ x['name'] for x in body['servers']]
-
-    return body
+    return server_names, server_uuid
 
 def get_token(id,passwd):
-    global token
     data = \
         {"auth":
             {
@@ -55,32 +48,47 @@ def get_token(id,passwd):
     # pixed header
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
     # TODO get project id
-
     res = requests.post(url_base + 'identity/v3/auth/tokens', headers=headers, data=json.dumps(data), verify=True)
     token = res.headers['X-Subject-Token']
-    projectID = get_projectID()[0]
+    return token
+
+def get_other_token(id, passwd, projectID):
+    data = \
+        {"auth":
+            {
+                "identity":
+                    {"password":
+                         {"user":
+                              {"domain":
+                                   {"name": "Default"},
+                               "password": passwd,
+                               "name": id
+                               }
+                          },
+                     "methods": ["password"]
+                     }
+            }
+        }
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
     data['auth']['scope'] = {
         "project":
             {"id": projectID }
     }
+
     res = requests.post(url_base + 'identity/v3/auth/tokens', headers=headers, data=json.dumps(data), verify=True)
     token = res.headers['X-Subject-Token']
-    return
 
-def get_resource_list(instance_uuid):
-    global token
+    return token
 
+def get_resource_list(token, instance_uuid):
     url = url_base + "metric/v1/resource/generic/%s"%(instance_uuid)
     headers = {'Content-Type': 'application/json, */*', 'X-Auth-Token':token}
     res = requests.get( url, headers = headers )
-    #print(res)
-    #print(res.json())
     body = res.json()
     return body
 
-def get_mesuare_list(body):
-    global token
-
+def get_mesuare_list(token, body):
     now = datetime.datetime.now()
     five_mins = datetime.timedelta(minutes=5)
     five_mins_ago = now - five_mins
@@ -91,19 +99,35 @@ def get_mesuare_list(body):
     disk = 'disk.usage'
     url = url_base + 'metric/v1/metric/%s/measures'%(body['metrics'][cpu])
     res = requests.get(url = url, headers = headers, params= PARAMS )
-    print(str(res.json()[-1][2]) + " %")
+    cpu = res.json()[-1][2]
 
     url = url_base + 'metric/v1/metric/%s/measures' % (body['metrics'][memory])
     res = requests.get(url=url, headers=headers, params=PARAMS)
-    print(str(res.json()[-1][2]/(1024) )+ " GB")
+    memory = res.json()[-1][2]/(1024)
 
     url = url_base + 'metric/v1/metric/%s/measures' % (body['metrics'][disk])
     res = requests.get(url=url, headers=headers, params=PARAMS)
-    print(str(res.json()[-1][2]/(8*1024*1024*1024)) + " GB")
+    disk = res.json()[-1][2]/(8*1024*1024*1024)
+    return cpu, memory, disk
+
 #'''openstack metric show'''
 
+def get_server_info(token,server_uuid):
+    headers = {'Content-Type': 'application/json', 'X-Auth-Token': token}
+    url = url_base + 'compute/v2.1/servers/%s'%server_uuid
+    res = requests.get(url=url, headers=headers)
+    return res.json()
+
+def get_flavor_info(token, flavorID):
+    headers = {'Content-Type': 'application/json', 'X-Auth-Token': token}
+    url = url_base + 'compute/v2.1/os-simple-tenant-usage/%s' % flavorID
+    res = requests.get(url=url, headers=headers)
+    print(res.__dict__)
+    #return res.json()
+
 if __name__ == '__main__':
-    get_token('demo','devstack')
-    get_projectID()
-    print(token)
-    get_server_list()
+    token = get_token('admin','devstack')
+    get_projectID(token)
+    get_server_list(token)
+    print(server_uuid)
+    get_server_info(token)
